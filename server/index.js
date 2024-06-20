4565321244cb4f796071ac113d9673fba9b7edc6
mesgna <mesgna.mehari8@gmail.com>
2024-06-20 15:39:50 +0100

    refactored

--- server/index.js
+++ server/index.js
@@ -1,52 +1,53 @@
-import express from "express"; 
-import dotenv from "dotenv"; 
-import helmet from "helmet"; 
-import morgan from "morgan"; 
-import mongoose from "mongoose"; 
-import userRoute from "./routes/user.js"; 
-import entryRoute from "./routes/entry.js"; 
-import cookieParser from "cookie-parser"; 
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import userRoute from "./routes/user.js";
+import entryRoute from "./routes/entry.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express(); 
-dotenv.config(); 
+const app = express();
+dotenv.config();
 
-const PORT = process.env.PORT || 5500; 
+const PORT = process.env.PORT || 5500;
 
-const connect = async () => { 
-try { 
-	await mongoose.connect(process.env.MONGO); 
-	console.log("Connected to mongoDB."); 
-} catch (error) { 
-	throw error; 
-} 
-}; 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
 
-mongoose.connection.on("disconnected", () => { 
-console.log("mongoDB disconnected!"); 
-}); 
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
 
-app.get('/', (req, res) => { res.send('Hello from Express!') }); 
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
 
-//middlewares 
-app.use(cookieParser()) 
-app.use(express.json()); 
-app.use(helmet()); 
+//middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.use(cors({ 
-origin: "http://localhost:3000", 
-credentials: true
-})) 
+app.use(morgan("common"));
 
+app.use("/api/users", userRoute);
+app.use("/api/entries", entryRoute);
 
-
-app.use(morgan("common")); 
-
-app.use("/api/users", userRoute); 
-app.use("/api/entries", entryRoute); 
-
-app.listen(PORT, () => { 
-console.log("Listening on port 5500"); 
-connect(); 
+app.listen(PORT, () => {
+  console.log("Listening on port 5500");
+  connect();
 });
--- server/routes/entry.js
+++ server/routes/entry.js
@@ -1,18 +1,18 @@
-import express from "express"; 
-import { 
-	createEntry, 
-	deleteEntry, 
-	getEntries, 
-	updateEntry, 
-	getEntry, 
-} from "../controllers/entry.js"; 
+import express from "express";
+import {
+  createEntry,
+  deleteEntry,
+  getEntries,
+  updateEntry,
+  getEntry,
+} from "../controllers/entry.js";
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.post("/", createEntry); 
-router.put("/:id", updateEntry); 
-router.delete("/:id", deleteEntry); 
-router.get("/author/:userId", getEntries); 
-router.get("/:id", getEntry) 
+router.post("/", createEntry);
+router.put("/:id", updateEntry);
+router.delete("/:id", deleteEntry);
+router.get("/author/:userId", getEntries);
+router.get("/:id", getEntry);
 
 export default router;
