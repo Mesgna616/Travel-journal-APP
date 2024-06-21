cf64696af6ad8d12b2d5452077b923d385ca9db4
mesgna <mesgna.mehari8@gmail.com>
2024-06-21 21:19:53 +0100

    clean-up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,3 @@
-// client/src/pages/Home.jsx
-
 import React, { useContext, useState } from "react";
 import Navbar from "../components/Navbar";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
@@ -18,7 +16,7 @@ const Home = () => {
 
   const search = (data) => {
     return data.filter((item) =>
-      keys.some((key) => item[key] && item[key].toLowerCase().includes(query))
+      keys.some((key) => item[key] && item[key].toLowerCase().includes(query.toLowerCase()))
     );
   };
 
@@ -41,31 +39,27 @@ const Home = () => {
 
       <div className="searchedPosts">
         {loading ? (
-          <>
-            <div
-              className="p"
-              style={{
-                color: "white",
-                fontFamily: "'Kaushan Script', cursive",
-              }}
-            >
-              Loading...
-            </div>
-          </>
+          <div
+            className="p"
+            style={{
+              color: "white",
+              fontFamily: "'Kaushan Script', cursive",
+            }}
+          >
+            Loading...
+          </div>
         ) : (
-          <>
-            {search(data)?.map((item, i) => (
-              <Card
-                key={i} // Remember to add a unique key
-                _id={item._id}
-                photos={item.photos}
-                title={item.title}
-                date={item.date}
-                location={item.location}
-                text={item.text}
-              />
-            ))}
-          </>
+          search(data)?.map((item) => (
+            <Card
+              key={item._id} // Use the unique _id property as the key
+              _id={item._id}
+              photos={item.photos}
+              title={item.title}
+              date={item.date}
+              location={item.location}
+              text={item.text}
+            />
+          ))
         )}
       </div>
     </div>
